75d9f7e198eb3e474d1bc80f7be10b64fcf80d75
Khairi Ibnutama <mr.ibnutama@gmail.com>
2022-03-15 22:07:48 +0700

    add username alpha_num validation to teacher registrasi

--- app/Http/Livewire/Registrasi/Check.php
+++ app/Http/Livewire/Registrasi/Check.php
@@ -15,44 +15,49 @@ class Check extends Component
 {
 	public $nig, $register = false;
 	public $name, $username, $email, $phone, $gender = 1, $password, $password_confirmation;
-	
-	public function checking(){
+
+	public function checking()
+	{
 		$this->validate(['nig' => 'required'], ['nig.required' => 'NIG tidak boleh kosong.']);
-		
+
 		$check = TeacherNig::where('number', $this->nig)->first();
-		
-		if(!$check){
+
+		if (!$check) {
 			$this->addError('nig', 'Nomor Induk Guru tidak terdaftar.');
-		} elseif($check->user_id != null){
+		} elseif ($check->user_id != null) {
 			$this->addError('nig', 'NIG yang anda masukkan sudah terdaftar pada akun lain.');
 		} else {
 			$this->register = true;
 		}
 	}
-	
-	public function register(){
-		$this->validate([
-			'name'	=> 'required',
-			'username'	=> 'required|min:4|unique:users',
-			'email'	=> 'required|email|unique:users',
-			'phone'	=> 'required|min:10|unique:userprofiles',
-			'password'	=> 'required|confirmed|min:6'
-		],
-		[
-			'name.required'	=> 'Nama tidak boleh kosong.',
-			'username.required'	=> 'Username tidak boleh kosong.',
-			'username.min'	=> 'Username minimal 4 karakter.',
-			'username.unique'	=> 'Username sudah terdaftar pada akun lain.',
-			'email.required'	=> 'Email tidak boleh kosong.',
-			'email.email'	=> 'Email tidak valid.',
-			'email.unique'	=> 'Email sudah terdaftar pada akun lain.',
-			'phone.required'	=> 'Nomor telepon tidak boleh kosong.',
-			'phone.min'	=> 'Nomor telepon tidak valid.',
-			'phone.unique'	=> 'Nomor telepon sudah terdaftar pada akun lain.',
-			'password.required'	=> 'Password tidak boleh kosong.',
-			'password.confirmed'	=> 'Konfirmasi password tidak sama.',
-			'password.min'	=> 'Password minimal 6 karakter.'
-		]);
+
+	public function register()
+	{
+		$this->validate(
+			[
+				'name'	=> 'required',
+				'username'	=> 'required|alpha_num|min:4|unique:users',
+				'email'	=> 'required|email|unique:users',
+				'phone'	=> 'required|min:10|unique:userprofiles',
+				'password'	=> 'required|confirmed|min:6'
+			],
+			[
+				'name.required'	=> 'Nama tidak boleh kosong.',
+				'username.required'	=> 'Username tidak boleh kosong.',
+				'username.alpha_num'	=> 'Username hanya terdiri dari huruf dan angka.',
+				'username.min'	=> 'Username minimal 4 karakter.',
+				'username.unique'	=> 'Username sudah terdaftar pada akun lain.',
+				'email.required'	=> 'Email tidak boleh kosong.',
+				'email.email'	=> 'Email tidak valid.',
+				'email.unique'	=> 'Email sudah terdaftar pada akun lain.',
+				'phone.required'	=> 'Nomor telepon tidak boleh kosong.',
+				'phone.min'	=> 'Nomor telepon tidak valid.',
+				'phone.unique'	=> 'Nomor telepon sudah terdaftar pada akun lain.',
+				'password.required'	=> 'Password tidak boleh kosong.',
+				'password.confirmed'	=> 'Konfirmasi password tidak sama.',
+				'password.min'	=> 'Password minimal 6 karakter.'
+			]
+		);
 		$user = User::create([
 			'name'	=> $this->name,
 			'email'	=> $this->email,
@@ -80,10 +85,9 @@ class Check extends Component
 		session()->flash('success', 'Registrasi berhasil, silahkan login.');
 		return redirect()->route('login');
 	}
-	
+
 	public function render()
 	{
 		return view('livewire.registrasi.check');
 	}
-	
 }
